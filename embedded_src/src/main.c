187d9d0254f9a2c2d49e1696ccd2d280029b6517
Brian Tate <brian.c.tate@gmail.com>
2025-11-17 07:20:30 -0600

    add adc channel placeholders in the device tree - doesn't compile

--- embedded_src/src/main.c
+++ embedded_src/src/main.c
@@ -1,9 +1,17 @@
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/sys/printk.h>
+#include <zephyr/drivers/adc.h>
 #include <zephyr/drivers/pwm.h>
 
 #define STEP PWM_USEC(100)
+
+#define TOUCHSCREEN_CENTER (1615)
+
+#define SERVO_MID_US (1439)
+#define SERVO_STEP (SERVO_MID_US - 10)
+
+#define ADC_NODE DT_NODELABEL(adc0)
 
 enum direction {
 	DOWN,
@@ -13,10 +21,12 @@ enum direction {
 //Servos
 static const struct pwm_dt_spec servo_left = PWM_DT_SPEC_GET(DT_NODELABEL(servo_left));
 static const struct pwm_dt_spec servo_right = PWM_DT_SPEC_GET(DT_NODELABEL(servo_right));
+//Note - min and max are the same for both servos
 static const uint32_t min_pulse = DT_PROP(DT_NODELABEL(servo_left), min_pulse);
 static const uint32_t max_pulse = DT_PROP(DT_NODELABEL(servo_left), max_pulse);
 
 //ADC's
+static const struct device *adc_dev = DEVICE_DT_GET(ADC_NODE);
 
 int main(void)
 {
@@ -25,7 +35,7 @@ int main(void)
 	enum direction dir = UP;
 	int ret;
 
-	printk("Servomotor control\n");
+	printk("Amazing Balancing Ball!!!\r\n");
 
 	if (!pwm_is_ready_dt(&servo_left)) {
 		printk("Error: PWM device %s is not ready\n",
